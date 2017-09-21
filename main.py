54269bb556ed088e90a762750a7c7c223059dcb2
hsiao-chi Wu <ciyalnin@gmail.com>
2017-09-21 11:23:45 +0800

    Update main.py
    eacho

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def worker():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='僕と契約して、魔法少女になってよ！'))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
